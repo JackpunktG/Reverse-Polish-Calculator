e3dfbf080ed91843d6c455f4eaf6cb50bd926a6b
Jack B <jack.goldsbrough@outlook.com>
2025-04-04 21:52:17 +0200

    working on getting multi digit numbers

--- RPC.c
+++ RPC.c
@@ -11,10 +11,10 @@ int rpc(int digit[], char operator[], int count)
 	int sum = 0;
 	int a = 0;
 
-	
-		digit1 = digit[a++];
-		digit2 = digit[a++];
-		operator_sign = operator[o++];
+
+		digit1 = digit[0];
+		digit2 = digit[1];
+		operator_sign = operator[1];
 		printf("%d - digit1\n", digit1);
 		printf("%d - digit2\n", digit2);
 		printf("%c - operator\n", operator);
@@ -50,25 +50,39 @@ int main()
 	char operator[MAX];
 	int digit_index = 0;
 	int operator_index = 0;
-	int count = 0;
+	int digit_count = 0;
 	int sum = 0;
 	int options = 0;
 
 
-	printf("The most basic version, only input two digits followed by operators... danke des!!!\n\n");	
+	printf("The most basic version, only input two digits followed by operators... danke des!!!\n\n");
 	while ((c = getchar()) != EOF) {
 		printf("in while\n");
-		if (c >= '0' <= '9')
-			digit[digit_index++] = c - '0';
-			count++;
-		if (c == '+' || c == '-' || c == '/' || c == '*')
-			operator[operator_index++] = c;
-			count++;
-			if (c == '\n')
-				break;
+		if (c >= '0' <= '9') {
+			if (digit_count > 0)
+				digit[digit_index] = digit[digit_index] + (c - '0');
+			else
+				digit[digit_index] = c - '0';
+				digit_count++;
 		}
-		sum = rpc(digit, operator, count);
+		if (c == '+' || c == '-' || c == '/' || c == '*') {
+			operator[operator_index++] = c;
+			digit_count = 0;
+			digit_index++;
+		}
+		if (c == ' ' || c == '\t') {
+			digit_count = 0;
+			digit_index++;
+		}
+		if (c == '\n') {
+			puts(digit);
+			digit_count = 0;
+			digit_index = 0;
+			break;
+		}
+		sum = rpc(digit, operator, digit_count);
 		printf("%d", sum);
-		
 
+
+	}
 }
